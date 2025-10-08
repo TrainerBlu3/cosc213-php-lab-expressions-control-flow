701a7e04e85885cb3a41e3265b212cf5dcbbebe8
Shire-Bennett <stefanwells123@gmail.com>
2025-10-08 16:29:13 -0700

    Update 02_branching.php

--- lab4/02_branching.php
+++ lab4/02_branching.php
@@ -1,1 +1,9 @@
+<?php
+// Input via query string: e.g., ?role=admin&day=Sat&code=404
+$role = $_GET['role'] ?? 'guest';
+$day = $_GET['day'] ?? 'Mon';
+$code = (int)($_GET['code'] ?? 200);
 
+// A) Role greeting with if/elseif/else
+if ($role === 'admin') {
+  echo "Welcome, admin\n"
